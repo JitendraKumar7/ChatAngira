742baa56af173cceb90e069e5c45a0184e52aafb
Angira <jitendrasoam90@gmail.com>
2018-09-17 18:03:36 +0530

    Initial commit

---
+++ app/src/main/java/com/chat/angira/model/Group.java
@@ -0,0 +1,12 @@
+package com.chat.angira.model;
+
+
+
+public class Group extends Room{
+    public String id;
+    public ListFriend listFriend;
+
+    public Group(){
+        listFriend = new ListFriend();
+    }
+}
